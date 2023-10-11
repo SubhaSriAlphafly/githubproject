810a99569bad354ec7b6069a3945115782129636
SubhaSriAlphafly <147132469+SubhaSriAlphafly@users.noreply.github.com>
2023-10-11 14:08:37 +0530

    Update Fibanocci series.c

--- Fibanocci series.c
+++ Fibanocci series.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 int main()
+{
+	int n,i,a=0,b=1,c;
+	printf("Enter no.of terms to be printed(n>2):");
+	scanf("%d", &n);
+	printf("Fibanocci Series:0 1");
+	for(i=1;i<=n-2;i++)
+	{
+	    c=a+b;
+	    a=b;
+	    b=c;
+	    printf(" %d", c);
+	}
+	return 0;
+}
+
